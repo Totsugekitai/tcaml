cdf67759326ad0aa18cf4d3e7fe6aebbe5a02991
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2021-10-04 13:17:25 +0900

    wip

--- src/eval.ml
+++ src/eval.ml
@@ -11,86 +11,68 @@ let rec lookup x env =
   | [] -> failwith ("unbound variable: " ^ x)
   | (y, v) :: tl -> if x = y then v else lookup x tl
 
-let rec eval e env =
-  let binop f e1 e2 env =
-    match (eval e1 env, eval e2 env) with
+let rec is_same_list l1 l2 =
+  if List.length l1 = List.length l2 then
+    match (l1, l2) with
+    | [], [] -> true
+    | IntVal hd1 :: tl1, IntVal hd2 :: tl2 -> hd1 = hd2 && is_same_list tl1 tl2
+    | BoolVal hd1 :: tl1, BoolVal hd2 :: tl2 ->
+        hd1 = hd2 && is_same_list tl1 tl2
+    | ListVal hd1 :: tl1, ListVal hd2 :: tl2 ->
+        is_same_list hd1 hd2 && is_same_list tl1 tl2
+    | _ -> false
+  else false
+
+let rec eval e env cont =
+  let binop f e1 e2 env cont =
+    match (eval e1 env cont, eval e2 env cont) with
     | IntVal n1, IntVal n2 -> IntVal (f n1 n2)
     | _ -> failwith "integer value expected"
   in
-  let rec is_same_list l1 l2 =
-    if List.length l1 = List.length l2 then
-      match (l1, l2) with
-      | [], [] -> true
-      | IntVal hd1 :: tl1, IntVal hd2 :: tl2 ->
-          hd1 = hd2 && is_same_list tl1 tl2
-      | BoolVal hd1 :: tl1, BoolVal hd2 :: tl2 ->
-          hd1 = hd2 && is_same_list tl1 tl2
-      | ListVal hd1 :: tl1, ListVal hd2 :: tl2 ->
-          is_same_list hd1 hd2 && is_same_list tl1 tl2
-      | _ -> false
-    else false
-  in
   match e with
   (* リテラル *)
-  | IntLit n -> IntVal n
-  | BoolLit b -> BoolVal b
+  | IntLit n -> cont (IntVal n)
+  | BoolLit b -> cont (BoolVal b)
   (* 算術演算 *)
-  | Plus (e1, e2) -> binop ( + ) e1 e2 env
-  | Minus (e1, e2) -> binop ( - ) e1 e2 env
-  | Times (e1, e2) -> binop ( * ) e1 e2 env
+  | Plus (e1, e2) -> binop ( + ) e1 e2 env cont
+  | Minus (e1, e2) -> binop ( - ) e1 e2 env cont
+  | Times (e1, e2) -> binop ( * ) e1 e2 env cont
   | Div (e1, e2) ->
-      if eval e2 env = IntVal 0 then failwith "zero division"
-      else binop ( / ) e1 e2 env
+      if eval e2 env cont = IntVal 0 then failwith "zero division"
+      else binop ( / ) e1 e2 env cont
   (* 比較 *)
-  | Eq (e1, e2) -> (
-      match (eval e1 env, eval e2 env) with
-      | IntVal n1, IntVal n2 -> BoolVal (n1 = n2)
-      | BoolVal b1, BoolVal b2 -> BoolVal (b1 = b2)
-      | ListVal l1, ListVal l2 -> BoolVal (is_same_list l1 l2)
-      | _ -> failwith "wrong value")
-  | InEq (e1, e2) -> (
-      match (eval e1 env, eval e2 env) with
-      | IntVal n1, IntVal n2 -> BoolVal (n1 <> n2)
-      | BoolVal b1, BoolVal b2 -> BoolVal (b1 <> b2)
-      | ListVal l1, ListVal l2 -> BoolVal (not (is_same_list l1 l2))
-      | _ -> failwith "wrong value")
-  | Greater (e1, e2) -> (
-      match (eval e1 env, eval e2 env) with
-      | IntVal n1, IntVal n2 -> BoolVal (n1 > n2)
-      | _ -> failwith "wrong value")
-  | Less (e1, e2) -> (
-      match (eval e1 env, eval e2 env) with
-      | IntVal n1, IntVal n2 -> BoolVal (n1 < n2)
-      | _ -> failwith "wrong value")
+  | Eq (e1, e2) ->
+      eval e1 env (fun v1 ->
+          eval e2 env (fun v2 -> cont (BoolVal (eq_val v1 v2))))
+  | InEq (e1, e2) ->
+      eval e1 env (fun v1 ->
+          eval e2 env (fun v2 -> cont (BoolVal (not (eq_val v1 v2)))))
+  | Greater (e1, e2) ->
+      eval e1 env (fun v1 ->
+          eval e2 env (fun v2 -> cont (BoolVal (greater v1 v2))))
+  | Less (e1, e2) ->
+      eval e1 env (fun v1 ->
+          eval e2 env (fun v2 -> cont (BoolVal (less v1 v2))))
   (* 制御構文 *)
-  | If (e1, e2, e3) -> (
-      match eval e1 env with
-      | BoolVal true -> eval e2 env
-      | BoolVal false -> eval e3 env
-      | _ -> failwith "wrong value")
+  | If (e1, e2, e3) ->
+      eval e1 env (function
+        | BoolVal true -> eval e2 env cont
+        | BoolVal false -> eval e3 env cont
+        | _ -> failwith "wrong value")
   (* 環境 *)
-  | Var x -> lookup x env
+  | Var x -> cont (lookup x env)
   | Let (x, e1, e2) ->
-      let env1 = ext env x (eval e1 env) in
-      eval e2 env1
+      eval e1 env (fun v1 ->
+          let env1 = ext env x v1 in
+          eval e2 env1 cont)
   (* 再帰関数 *)
   | LetRec (f, x, e1, e2) ->
       let env1 = ext env f (RecFunVal (f, x, e1, env)) in
-      eval e2 env1
+      eval e2 env1 cont
   (* 関数抽象 *)
-  | Fun (x, e1) -> FunVal (x, e1, env)
-  (* 関数適用 *)
-  | App (e1, e2) -> (
-      let funpart = eval e1 env in
-      let arg = eval e2 env in
-      match funpart with
-      | FunVal (x, body, env1) ->
-          let env2 = ext env1 x arg in
-          eval body env2
-      | RecFunVal (f, x, body, env1) ->
-          let env2 = ext (ext env1 x arg) f funpart in
-          eval body env2
-      | _ -> failwith "wrong value in App")
+  | Fun (x, e1) -> cont (FunVal (x, e1, env))
+  | App (e1, e2) ->
+      eval e1 env (fun funpart -> eval e2 env (fun arg -> app funpart arg cont))
   (* リストに対する操作 *)
   | Empty -> ListVal []
   | Cons (e1, e2) -> (
@@ -107,4 +89,30 @@ let rec eval e env =
       | ListVal [] -> ListVal []
       | ListVal (_ :: tl) -> ListVal tl
       | _ -> failwith "expected list, but other found")
+  | _ -> failwith "unexpected expr"
+
+and eq_val v1 v2 =
+  match (v1, v2) with
+  | IntVal n1, IntVal n2 -> n1 = n2
+  | BoolVal b1, BoolVal b2 -> b1 = b2
+  | ListVal l1, ListVal l2 -> is_same_list l1 l2
+  | _, _ -> failwith "unexpected expr"
+
+and greater v1 v2 =
+  match (v1, v2) with
+  | IntVal n1, IntVal n2 -> n1 > n2
+  | _, _ -> failwith "unexpected expr"
+
+and less v1 v2 =
+  match (v1, v2) with
+  | IntVal n1, IntVal n2 -> n1 < n2
+  | _, _ -> failwith "unexpected expr"
+
+(* 関数適用 *)
+and app funpart arg cont =
+  match funpart with
+  | FunVal (x, body, env1) -> eval body (ext env1 x arg) cont
+  | RecFunVal (f, x, body, env1) ->
+      let env2 = ext (ext env1 x arg) f funpart in
+      eval body env2 cont
   | _ -> failwith "unexpected expr"
